9b336ad6658664072e93fb71d08be4b218699c83
minjeong kim <6alswjd6@gmail.com>
2023-05-25 15:01:31 +0900

    feat: 페이지 네이션 추가

--- backend/cosmic_django/post/views.py
+++ backend/cosmic_django/post/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from .models import Post
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 # @login_required
@@ -12,7 +12,7 @@ def post_new(request):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False) #바로 저장하지 않는다
-            #post.user = request.user
+            #post.user = request.user #작성자 저장을 위함
             post.save()
             messages.success(request, "포스팅 저장 완료")
             return redirect(post)
@@ -30,7 +30,11 @@ def post_detail(request, pk):
     })
 
 def post_list(request):
-    postList = Post.objects.all()
+    page = request.GET.get('page', '1')
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
     return render(request, 'post_list.html', {
-        "postList" : postList
+        "page_obj" : page_obj,
     })
